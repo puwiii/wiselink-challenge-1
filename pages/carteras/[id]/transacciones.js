518cbda5f344cfc5370d354971f70e6341221d20
Fede <federicoameratte@gmail.com>
2023-03-18 04:12:00 -0300

    a

--- pages/carteras/[id]/transacciones.js
+++ pages/carteras/[id]/transacciones.js
@@ -166,7 +166,7 @@ function Transacciones({ data }) {
 
 export default Transacciones;
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const response = await fetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1"
   );
